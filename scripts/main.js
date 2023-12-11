195983e38a026480bca785bd708ff84f25114403
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-12-11 12:06:39 +0100

    .

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function () {
 	// Simula tiempo de carga
 	setTimeout(function () {
 		document.getElementById('loader').style.display = 'none';
-	}, 7000);
+	}, 2000);
 });
 
 window.onload = () => {
