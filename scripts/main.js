271bbd78809b1a9a70c02d3f39ddd8c78659b465
Cate <caterinailariopaz@alumnos.tuniversformacion.es>
2023-12-11 10:56:22 +0100

    corrección loader

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 window.addEventListener('DOMContentLoaded', function () {
+	// Muestra el loader
+    document.getElementById("loader").style.display = "flex";
+	// Simula tiempo de carga
 	setTimeout(function () {
 		document.getElementById('loader').style.display = 'none';
+		document.getElementById("main").style.display = "block";
 	}, 8000);
 });
 
